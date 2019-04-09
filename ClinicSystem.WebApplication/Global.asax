5c0e9e2178e698430bf76e76cf14dad715df72be
Karol Chłopik <chlopikk@essilor.com.pl>
2019-04-09 21:01:17 +0200

    Add project files.

---
+++ ClinicSystem.WebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClinicSystem.WebApplication.MvcApplication" Language="C#" %>
---
+++ ClinicSystem.WebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClinicSystem.WebApplication.Startup))]
+namespace ClinicSystem.WebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
